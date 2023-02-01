8e34e6024f7629f3639dda85f8e5f5d4db13f8fb
Dell <Dell@DESKTOP-S5F09UB>
2023-02-01 18:57:18 +0530

    changes

--- githubt546/Program.cs
+++ githubt546/Program.cs
@@ -7,6 +7,8 @@ namespace githubt546
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello jikl;!");
+
         }
     }
 }
