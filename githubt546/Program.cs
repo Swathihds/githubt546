ece80f534efd4ee815754221b7cb567dcc31479a
Dell <Dell@DESKTOP-S5F09UB>
2023-02-01 18:08:12 +0530

    Add project files.

---
+++ githubt546/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace githubt546
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
